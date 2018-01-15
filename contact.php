d907e962985ed975b5dab354e49d2c164ff9e2e1
Shubosha Kuro <shubosha.kuro@gmail.com>
2018-01-15 17:27:03 +0100

    post

--- contact.php
+++ contact.php
@@ -40,6 +40,7 @@ try
     if(count($_POST) == 0) throw new \Exception('Form is empty');
     
     $emailText = "You have a new message from your contact form\n=============================\n";
+    $errorMessage = "nothing :/";
 
     foreach ($_POST as $key => $value) {
         // If the field exists in the $fields array, include it in the email 
