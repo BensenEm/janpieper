2ca72c227a2fd55bc44980ad744dced9f3c37e96
Shubosha Kuro <shubosha.kuro@gmail.com>
2018-01-15 16:58:57 +0100

    email

--- contact.php
+++ contact.php
@@ -4,10 +4,12 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+// $from = 'Demo contact form <demo@domain.com>';
+$from = 'sa.bussian@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+// $sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'shubosha.kuro@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
